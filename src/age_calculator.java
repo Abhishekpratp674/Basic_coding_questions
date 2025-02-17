4855eec122e8f51c02ed40b507b9cc990dfcbc6a
Abhishek pratap <dummyhop674@gmail.com>
2025-02-17 22:08:17 +0530

    prob2 added

---
+++ src/age_calculator.java
@@ -0,0 +1,29 @@
+// take the input of name & age then print the age?
+
+
+import java.util.Scanner;
+
+
+public class age_calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        System.out.println(("Enter first number"));
+//        int b = sc.nextInt();
+//        System.out.println("Enter second number");
+//        System.out.println(a + b);
+
+//        Scanner scc = new Scanner(System.in);
+        System.out.println(("Enter name"));
+        String name = sc.nextLine();
+        System.out.println("Enter age");
+        int age = sc.nextInt();
+        System.out.println(" hello lucifer " + name + " you are " + age + " years old. ");
+
+
+
+
+
+    }
+}
