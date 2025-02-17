9c1bd1d17c5c2e529f6723d20073f23ea4846de1
Abhishek pratap <dummyhop674@gmail.com>
2025-02-17 22:03:30 +0530

    prob1 added

---
+++ src/additonOfNumber.java
@@ -0,0 +1,9 @@
+// addtion of two number
+
+public class additonOfNumber {
+    public static void main(String[] args) {
+        int number1 = 12;
+        int number2 = 9;
+        System.out.println("the number is " + number1 + " & " +number2 + " = " + (number1 + number2));
+    }
+}
